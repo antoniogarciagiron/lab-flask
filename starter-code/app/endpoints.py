7bfca31fb8eb6ee2c05bdebe993a16b54da5c69c
Antonio Garcia Giron <antoniogarciagiron@gmail.com>
2021-02-17 19:37:28 +0100

    Pues eso...

--- starter-code/app/endpoints.py
+++ starter-code/app/endpoints.py
@@ -7,10 +7,21 @@ from bson import json_util
 from app.helpers.mongoConnection import *
 
 
-
 @app.route("/celebrity/create")
 def create_celebrity():
-    celeb = request.args
-    
-    id_ = insert_celebrity(**celeb)
-    return json_util.dumps(id_)
+    celeb = request.args   
+    res = insert_celebrity(**celeb)
+    return json_util.dumps(res)
+
+
+@app.route("/celebrities")
+def show_all_celebrities():
+    res = fetch_celebrites_list()
+    return json_util.dumps(res)
+
+
+@app.route("/celebrities/details")
+def show_all_celebrities():
+    celeb_id = request.args
+    res = fetch_celebrity(**celeb_id)
+    return json_util.dumps(res)   
--- starter-code/app/helpers/mongoConnection.py
+++ starter-code/app/helpers/mongoConnection.py
@@ -10,6 +10,11 @@ def insert_celebrity(**celeb):
     return res.inserted_id
 
 
-def fetch_celebrity(**celeb):
-    res = celebrities.find({"name":celeb}, {"name":1, "occupation":1, "catch_phrase":1  ,"_id":0})
+def fetch_celebrity(**celeb_id):
+    res = celebrities.find({"_id":celeb_id}, {"name":1, "occupation":1, "catch_phrase":1  ,"_id":0})
+    return res
+
+
+def fetch_celebrites_list():
+    res = celebrities.find(celeb, {"name":1, "_id":1})
     return list(res)
