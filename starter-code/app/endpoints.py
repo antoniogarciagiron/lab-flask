e0f72aca453474558f1bd10952c19e2485eab3de
Antonio Garcia Giron <antoniogarciagiron@gmail.com>
2021-02-19 08:27:48 +0100

    Modificando cosas

--- starter-code/app/endpoints.py
+++ starter-code/app/endpoints.py
@@ -6,10 +6,24 @@ from flask import request
 from bson import json_util
 from app.helpers.mongoConnection import *
 
+def is_valid(input,keywords):
+    for key in keywords:
+        if key not in input.keys():
+            return False, {"Fail":f"Must pass {key}"}
+    return True, "OK"
+
+
+@app.route('/')
+def welcome():
+    return 'Welcome to the API'
+
 
 @app.route("/celebrity/create")
 def create_celebrity():
-    celeb = request.args   
+    celeb = request.args
+    check, msg = is_valid(celeb,["name","occupation", "catch_phrase"])
+    if not check:
+        return msg
     res = insert_celebrity(**celeb)
     return json_util.dumps(res)
 
@@ -21,7 +35,10 @@ def show_all_celebrities():
 
 
 @app.route("/celebrities/details")
-def show_all_celebrities():
+def show_celebrity():
     celeb_id = request.args
+    check, msg = is_valid(celeb_id,["_id"])
+    if not check:
+        return msg
     res = fetch_celebrity(**celeb_id)
     return json_util.dumps(res)   
